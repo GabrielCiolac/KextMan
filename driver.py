5d59efda1a9f33dc006186aba328a1d201455df0
Gabriel Ciolac <gabrielciolac@gmail.com>
2020-11-24 14:45:18 -0500

    mount_partition boiler plate

--- driver.py
+++ driver.py
@@ -1,5 +1,1 @@
 from mount_parition import Mounter
-
-mnt = Mounter()
-
-print(mnt.check_os())
--- mount_parition.py
+++ mount_parition.py
@@ -4,6 +4,22 @@ class Mounter():
     def __init__(self):
         pass
     
-    def check_os(self)->bool:
+    def __check_os(self)->bool:
         return platform.system() == 'Darwin'
+
+    def __mount_efi(self):
+        pass
+
+    def __set_OC_env(self):
+        pass
+
+    def __controller(self):
+        if self.__check_os:
+            self.__mount_efi()
+        else:
+            self.__set_OC_env()
+        self.path = ''
+
+    def get_path(self):
+        return self.path
 
