b2be61d267d946c7700743854e9b007dc76072e3
Gabriel Ciolac <gabrielciolac@gmail.com>
2020-11-24 19:40:52 -0500

    Mounting EFI Partition

--- mount_parition.py
+++ mount_parition.py
@@ -18,10 +18,71 @@ class Mounter():
     def __check_os(self)->bool:
         return platform.system() == 'Darwin'
 
+
+    '''
+        parses the identifer and returns the EFI partition identifer
+
+        1) Check to see if disk is APFS
+        2) If it is APFS partition get the phsyical disk number (APFS phsyical store)
+        3) Use diskutil info diskn to get the partitions of the disk
+        4) return diskiddentifier of EFI
+    '''
+    def __parse_identifier(self,raw:str)->str:
+        id_list = raw.splitlines()
+        disk_of_vol = None
+        #goes through all the lines and grabs relevent information
+        for s in id_list:
+            if 'Part of Whole:' in s:
+                disk_of_vol = s.split(' ')
+                disk_of_vol = set(disk_of_vol) #removes repeating char
+            if 'APFS Physical Store:' in s: #if it's an APFS partition rewrite
+                disk_of_vol = s.split(' ')
+                disk_of_vol = set(disk_of_vol)
+
+        if disk_of_vol is None:
+            return None
+        
+        disk_num = None
+        for s in disk_of_vol: #gets disk number
+            if 'disk' in s:
+                disk_num = s
+                break
+        
+        if disk_num is None:
+            return None
+        try: #drops the sector number if it exists
+            split = disk_num.split('s') 
+            disk_num = split[0] + 's' + split[1]
+        except:
+            pass
+
+        raw_disk_info = os.popen('diskutil list ' + disk_num).read() #gets the disk info
+        disk_info = raw_disk_info.splitlines()
+
+        efi_data = None
+        for s in disk_info:
+            if 'EFI' in s:
+                efi_data = set(s.split(' ')) #gets identifier for the EFI partition
+                break
+        if efi_data is None:
+            return None
+        for s in efi_data:
+            if 'disk' in s:
+                return s #returns disk number of the EFI partition
+
+
+
+        return None
+            
+
+
     '''
         Lists available disks, and asks user which EFI partition they'd like to mount
 
         If an OpenCore enviroment exists in the EFI partition that enviroment will be set as the working partition.
+
+        A special thanks to corpnewt's MountEFI for some inspiration, corpnewt's project can be found here:
+        https://github.com/corpnewt/MountEFI
     '''
     def __mount_efi(self)->bool:
         while self.__path is None:
@@ -40,8 +101,20 @@ class Mounter():
 
             if sel is None:
                 return False
-            
-            
+
+            #insures user inputed an integer, and that integer is in the list
+            try:
+                sel = int(sel)
+                mnt_pnt = dsk_lst[sel]
+            except:
+                print('Unable to find selected')
+                return False
+
+            raw_id = os.popen('diskutil info ' + mnt_pnt).read()
+            parsed_id = self.__parse_identifier(raw_id)
+
+            os.system('sudo diskutil mount '+parsed_id)
+
 
             return True
 
